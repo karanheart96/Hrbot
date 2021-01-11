dafa1830a509708542542769ea3cee9f5b7c7f08
Glitch (hello-express) <none>
2021-01-11 18:34:12 +0000

    🙈💴 Checkpoint ./app.js:27250835/985

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app = new App({
 });
 
 
+const oauth2Client = new google.auth.OAuth2(
+  YOUR_CLIENT_ID,
+  YOUR_CLIENT_SECRET,
+  YOUR_REDIRECT_URL
+);
+
+const url = oauth2Client.generateAuthUrl({
+  scope: 'https://www.googleapis.com/auth/calendar'
+});
+
 
 // All the room in the world for your code
 app.event('app_home_opened', async ({ event, client, context }) => {
