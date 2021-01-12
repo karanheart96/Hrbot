210a7071d2d651566be8ad66f66d6f3522768600
Glitch (hello-express) <none>
2021-01-12 03:05:09 +0000

    👮🏩 Checkpoint ./credentials.json:27250835/1505 ./app.js:27250835/89

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const {client_secret, client_id, redirect_uris} = credentials.web;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
@@ -258,4 +258,5 @@ function listMajors(auth) {
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
+  console.log(listMajors());
 })();
